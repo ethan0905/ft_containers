cf71fc41914e12c0d852586d105489fa756f3aa3
Ethan SAFAR <esafar@e1r11p9.clusters.42paris.fr>
2022-10-26 18:12:38 +0200

    playing with std::vector.insert() function

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:36:39 by esafar            #+#    #+#             */
-/*   Updated: 2022/10/26 18:00:55 by esafar           ###   ########.fr       */
+/*   Updated: 2022/10/26 18:11:45 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #include <iostream>
 #include <string>
 
-// test using std::vector
+// test std::vector
 int main(int ac, char **av)
 {
+    // using ::iterator and ::reverse_iterator
     std::vector<int> v;
     std::vector<int>::iterator it;
     std::vector<int>::reverse_iterator rit;
@@ -54,5 +55,27 @@ int main(int ac, char **av)
         std::cout << *rit << std::endl;
         rit++;
     }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    // using insert() function
+    std::vector<std::string> v2;
+    std::vector<std::string>::iterator it2;
+    
+    v2.push_back("Hello");
+    v2.push_back("World");
+    v2.push_back("!");
+    
+    it2 = v2.begin();
+    it2++;
+    v2.insert(it2, "C++");
+    it2 = v2.begin();
+    while (it2 != v2.end())
+    {
+        std::cout << *it2 << std::endl;
+        it2++;
+    }
+    
     return (0);
 }
