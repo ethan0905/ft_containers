bf88867761f9fd1ab9a8beca8a312af2574deeb9
Ethan SAFAR <esafar@e1r11p9.clusters.42paris.fr>
2022-10-26 18:01:42 +0200

    working around with std::vector.capacity()

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:36:39 by esafar            #+#    #+#             */
-/*   Updated: 2022/10/26 17:46:52 by esafar           ###   ########.fr       */
+/*   Updated: 2022/10/26 18:00:55 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,25 @@ int main(int ac, char **av)
     std::vector<int>::reverse_iterator rit;
 
     v.push_back(1);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(2);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(3);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(4);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(5);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(6);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(7);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(8);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(9);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
     v.push_back(10);
+    std::cout << "Max capacity: " << v.capacity() << std::endl;
 
     it = v.begin();
     rit = v.rbegin();
