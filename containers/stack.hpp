199abc417c1273ed861c0b9840cc21c304ebba56
Ethan SAFAR <esafar@e1r8p10.clusters.42paris.fr>
2022-11-04 17:16:51 +0100

    implemented std::stack container and appropriate test using std::stack and ft::stack

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 14:17:23 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/04 15:01:35 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/04 17:00:28 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,63 @@
 # include <iostream>
 # include <vector>
 
+// typedef allows us to give a new name to an existing data type
+// explicit keyword allows only direct-initialization (avoid implicit conversions and copy initialization from braced-init-list)
+// friend keyword allows a function to access private and protected members of a class
+
 namespace ft
 {
-    
+    template <class T, class Container = std::vector<T> >
+    class stack
+    {
+        public:
+            typedef T value_type;
+            typedef Container container_type;
+            // typedef size_t size_type;
+
+        private:
+            container_type _container;
+
+        public:
+            explicit stack(const container_type& ctnr = container_type()) : _container(ctnr) {}
+            bool empty() const {
+                return (_container.empty());
+            }
+            size_t size() const {
+                return (_container.size());
+            }
+            value_type& top() {
+                return (_container.back());
+            }
+            const value_type& top() const {
+                return (_container.back());
+            }
+            void push(const value_type& val) {
+                _container.push_back(val);
+            }
+            void pop() {
+                _container.pop_back();
+            }
+            
+            friend bool operator==(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+                return (lhs._container == rhs._container);
+            }
+            friend bool operator!=(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+                return (lhs._container != rhs._container);
+            }
+            friend bool operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+                return (lhs._container < rhs._container);
+            }
+            friend bool operator<=(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+                return (lhs._container <= rhs._container);
+            }
+            friend bool operator>(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+                return (lhs._container > rhs._container);
+            }
+            friend bool operator>=(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+                return (lhs._container >= rhs._container);
+            }
+    };
 }
 
 #endif
--- containers/test.cpp
+++ containers/test.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:00:54 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/04 16:30:42 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/04 17:13:28 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,82 @@
 #include <stack>
 #include <vector>
 
+#include "stack.hpp"
+
 int main(void)
 {
     std::cout << std::boolalpha;
 
-    std::cout << "==============STD::STACK==============" << std::endl;
-    // test std::stack
+    std::cout << "==============STD::STACK<int>==============" << std::endl;
     
-    std::stack<int> s;
-    
-    std::cout << "stack.empty(): " << s.empty() << std::endl << std::endl;
+    std::stack<int> stack_std;
+    ft::stack<int> stack_ft;
 
-    for (int i = 0; i < 10; i++)
-        s.push(i);
+    std::cout << "std::stack.empty(): " << stack_std.empty() << std::endl;
+    std::cout << "ft::stack.empty(): " << stack_ft.empty() << std::endl << std::endl;
+
+    for (int i = 0; i <= 10; i++)
+        stack_std.push(i);
     
-    std::cout << "stack.top(): " << s.top() << std::endl;
-    std::cout << "stack.size(): " << s.size() << std::endl << std::endl;
-    std::cout << "stack.pop()" << std::endl << std::endl;
-    s.pop();
-    std::cout << "stack.top(): " << s.top() << std::endl;
-    std::cout << "stack.size(): " << s.size() << std::endl << std::endl;
-    std::cout << "stack.empty(): " << s.empty() << std::endl << std::endl;
+    for (int i = 0; i <= 10; i++)
+        stack_ft.push(i);
     
-    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "std::stack.top(): " << stack_std.top() << std::endl;
+    std::cout << "ft::stack.top(): " << stack_ft.top() << std::endl << std::endl;
+    
+    std::cout << "std::stack.size(): " << stack_std.size() << std::endl;
+    std::cout << "ft::stack.size(): " << stack_ft.size() << std::endl << std::endl;
+    
+    std::cout << "std::stack.pop()" << std::endl;
+    std::cout << "ft::stack.pop()" << std::endl << std::endl;
+    
+    stack_std.pop();
+    stack_ft.pop();
+    
+    std::cout << "std::stack.top(): " << stack_std.top() << std::endl;
+    std::cout << "ft::stack.top(): " << stack_ft.top() << std::endl << std::endl;
+    
+    std::cout << "std::stack.size(): " << stack_std.size() << std::endl;
+    std::cout << "ft::stack.size(): " << stack_ft.size() << std::endl << std::endl;
+    
+    std::cout << "std::stack.empty(): " << stack_std.empty() << std::endl;
+    std::cout << "ft::stack.empty(): " << stack_ft.empty() << std::endl << std::endl;
+    
+    std::cout << "==============STD::STACK<int, std::vector>==============" << std::endl;
         
-    // test std::stack with a vector
-    std::vector<int> v;
-    
+    std::vector<int> vector_std;
 
-    std::cout << "vector.empty(): " << v.empty() << std::endl << std::endl;
+    std::cout << "std::vector.empty(): " << vector_std.empty() << std::endl << std::endl;
+
+    for (int i = 0; i <= 10; i++)
+        vector_std.push_back(i);
     
-    for (int i = 0; i < 10; i++)
-        v.push_back(i);
+    std::stack<int, std::vector<int> > stack2_std(vector_std);
+    ft::stack<int, std::vector<int> > stack2_ft(vector_std);
+
+    std::cout << "std::stack.empty(): " << stack2_std.empty() << std::endl;
+    std::cout << "ft::stack.empty(): " << stack2_ft.empty() << std::endl << std::endl;
     
-    std::stack<int, std::vector<int> > s2(v);
+    std::cout << "std::stack.top(): " << stack2_std.top() << std::endl;
+    std::cout << "ft::stack.top(): " << stack2_ft.top() << std::endl << std::endl;
     
-    std::cout << "stack.top(): " << s2.top() << std::endl;
-    std::cout << "stack.size(): " << s2.size() << std::endl << std::endl;
-    std::cout << "stack.pop()" << std::endl << std::endl;
-    s2.pop();
-    std::cout << "stack.top(): " << s2.top() << std::endl;
-    std::cout << "stack.size(): " << s2.size() << std::endl << std::endl;
-    std::cout << "stack.empty(): " << s2.empty() << std::endl << std::endl;
+    std::cout << "std::stack.size(): " << stack2_std.size() << std::endl;
+    std::cout << "ft::stack.size(): " << stack2_ft.size() << std::endl << std::endl;
+    
+    std::cout << "std::stack.pop()" << std::endl;
+    std::cout << "ft::stack.pop()" << std::endl << std::endl;
+    
+    stack2_std.pop();
+    stack2_ft.pop();
+    
+    std::cout << "std::stack.top(): " << stack2_std.top() << std::endl;
+    std::cout << "ft::stack.top(): " << stack2_ft.top() << std::endl << std::endl;
+    
+    std::cout << "std::stack.size(): " << stack2_std.size() << std::endl;
+    std::cout << "ft::stack.size(): " << stack2_ft.size() << std::endl << std::endl;
+    
+    std::cout << "std::stack.empty(): " << stack2_std.empty() << std::endl;
+    std::cout << "ft::stack.empty(): " << stack2_ft.empty() << std::endl << std::endl;
 
     return (0);
 }
