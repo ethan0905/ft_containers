c99eefa9c9a29a6ee9d5bcb607cb24bf39427ca0
Ethan SAFAR <esafar@e1r8p10.clusters.42paris.fr>
2022-11-04 15:52:49 +0100

    cleaned test for std::stack inside main

--- containers/test.cpp
+++ containers/test.cpp
@@ -6,34 +6,36 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:00:54 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/04 15:35:07 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/04 15:51:35 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
-#include <vector>
-#include <list>
-#include <deque>
-
-// main that test the std::stack
+// #include <vector>
+// #include <list>
+// #include <deque>
 
 int main()
 {
-    std::stack<int> a;
+    std::cout << std::boolalpha;
+
+    std::cout << "==============STD::STACK==============" << std::endl;
     
+    std::stack<int> s;
+    
+    std::cout << "stack.empty(): " << s.empty() << std::endl << std::endl;
+
     for (int i = 0; i < 10; i++)
-        a.push(i);
+        s.push(i);
     
-    std::cout << "stack.top(): " << a.top() << std::endl;
-    std::cout << "stack.size(): " << a.size() << std::endl << std::endl;
+    std::cout << "stack.top(): " << s.top() << std::endl;
+    std::cout << "stack.size(): " << s.size() << std::endl << std::endl;
     std::cout << "stack.pop()" << std::endl << std::endl;
-    a.pop();
-    std::cout << "stack.top(): " << a.top() << std::endl;
-    std::cout << "stack.size(): " << a.size() << std::endl << std::endl;
-    std::cout << "stack.empty(): " << a.empty() << std::endl << std::endl;
-    std::cout << "stack.top(): " << a.top() << std::endl;
-    std::cout << "stack.size(): " << a.size() << std::endl;
+    s.pop();
+    std::cout << "stack.top(): " << s.top() << std::endl;
+    std::cout << "stack.size(): " << s.size() << std::endl << std::endl;
+    std::cout << "stack.empty(): " << s.empty() << std::endl << std::endl;
     
     return (0);
 }
