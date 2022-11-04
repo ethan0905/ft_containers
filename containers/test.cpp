b4d8134bcfe2fee7f5c35c57037afca255c8fe29
Ethan SAFAR <esafar@e1r8p10.clusters.42paris.fr>
2022-11-04 16:31:39 +0100

    implemented test using vector and stack

--- containers/test.cpp
+++ containers/test.cpp
@@ -6,21 +6,20 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:00:54 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/04 15:51:35 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/04 16:30:42 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
-// #include <vector>
-// #include <list>
-// #include <deque>
+#include <vector>
 
-int main()
+int main(void)
 {
     std::cout << std::boolalpha;
 
     std::cout << "==============STD::STACK==============" << std::endl;
+    // test std::stack
     
     std::stack<int> s;
     
@@ -37,5 +36,26 @@ int main()
     std::cout << "stack.size(): " << s.size() << std::endl << std::endl;
     std::cout << "stack.empty(): " << s.empty() << std::endl << std::endl;
     
+    std::cout << "--------------------------------------" << std::endl;
+        
+    // test std::stack with a vector
+    std::vector<int> v;
+    
+
+    std::cout << "vector.empty(): " << v.empty() << std::endl << std::endl;
+    
+    for (int i = 0; i < 10; i++)
+        v.push_back(i);
+    
+    std::stack<int, std::vector<int> > s2(v);
+    
+    std::cout << "stack.top(): " << s2.top() << std::endl;
+    std::cout << "stack.size(): " << s2.size() << std::endl << std::endl;
+    std::cout << "stack.pop()" << std::endl << std::endl;
+    s2.pop();
+    std::cout << "stack.top(): " << s2.top() << std::endl;
+    std::cout << "stack.size(): " << s2.size() << std::endl << std::endl;
+    std::cout << "stack.empty(): " << s2.empty() << std::endl << std::endl;
+
     return (0);
 }
