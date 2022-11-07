4d40ae5229371301d856f12fc8d46e26757fad80
Ethan SAFAR <esafar@e2r1p5.clusters.42paris.fr>
2022-11-07 11:41:32 +0100

    added template object to initialize vector class, based on how he is define in <vector>

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,19 +6,25 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:56:32 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/06 18:06:21 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/07 11:40:27 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-# include <iostream>
+#include <iostream>
+#include <memory>
 #include "stack.hpp"
+
+// std::allocator allows us to allocate memory for our vector and to deallocate
 
 namespace ft
 {
-    
+    template<class T, class Allocator = std::allocator<T>>
+    class vector {
+        
+    };
 }
 
 #endif
