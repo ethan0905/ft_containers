2446548071bd932771964efe2c8384edc221e1b9
Ethan SAFAR <esafar@e2r3p16.clusters.42paris.fr>
2022-11-08 16:35:18 +0100

    working on vector classs constructors, seems to compile and no segfaults

---
+++ containers/ftvector_test.cpp
@@ -0,0 +1,16 @@
+#include "vector.hpp"
+
+int main()
+{
+    ft::vector<int> vector_ft;
+
+/*    std::cout << "ft::vector.empty(): " << vector_ft.empty() << std::endl << std::endl;
+
+    for (int i = 0; i <= 10; i++)
+        vector_ft.push_back(i);
+    
+    ft::stack<int, ft::vector<int> > stack2_ft(vector_ft);
+
+    std::cout << "ft::stack.empty(): " << stack2_ft.empty() << std::endl << std::endl;
+*/    return (0);
+}
--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:56:32 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/07 14:31:05 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/08 16:34:10 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,30 @@
 #include <memory>
 #include "stack.hpp"
 
-// std::allocator allows us to allocate memory for our vector and to deallocate
+// std::allocator allows us to allocate memory for our vector and to deallocate         
 
 namespace ft
 {
-    template<class T, class Allocator = std::allocator<T>>
+    template<class T, class Allocator = std::allocator<T> >
     class vector {
         public:
             typedef T value_type;
             typedef Allocator allocator_type;
-            typedef typename Base::pointer  pointer;
+            typedef typename Allocator::pointer  pointer;
+            typedef typename Allocator::size_type size_type;
+            
         private:
+            allocator_type _alloc;
+            pointer _ptr;
+            size_type _size;
+            size_type _capacity;
+            
         public:
-            explicit vector(const allocator_type &alloc = Allocator()) : {}
+            // Constructors
+            explicit vector(const allocator_type& alloc = allocator_type()) : _alloc(alloc), _ptr(NULL), _size(0), _capacity(0) {}
+            explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _ptr(NULL), _size(0), _capacity(0) {
+                assign(n, val);
+            }
     };
 }
 
