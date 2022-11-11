117014f9d5452b37b73d0ecf829aa50896b9e6a3
Ethan SAFAR <esafar@e2r1p5.clusters.42paris.fr>
2022-11-11 18:20:12 +0100

    worked on constructors - need to fix iterator constructor that block compilation

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:56:32 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/08 16:34:10 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/11 18:16:42 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <memory>
 #include "stack.hpp"
+#include "../header/color.h"
 
 // std::allocator allows us to allocate memory for our vector and to deallocate         
 
@@ -31,15 +32,72 @@ namespace ft
             
         private:
             allocator_type _alloc;
-            pointer _ptr;
+            pointer _vector;
             size_type _size;
             size_type _capacity;
             
         public:
             // Constructors
-            explicit vector(const allocator_type& alloc = allocator_type()) : _alloc(alloc), _ptr(NULL), _size(0), _capacity(0) {}
-            explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _ptr(NULL), _size(0), _capacity(0) {
-                assign(n, val);
+            explicit vector(const allocator_type& alloc = allocator_type()) : _alloc(alloc), _vector(NULL), _size(0), _capacity(0) { std::cout << CYAN "Default constructor" END << std::endl; }
+            explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _size(n), _capacity(n), _vector(_alloc.allocate(n)){
+                std::cout << CYAN "Constructor with n and val" END << std::endl;
+                for (size_type i = 0; i < n; i++)
+                    _alloc.construct(_vector + i, val);
+            }
+            // template <class InputIterator>
+            // vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : _alloc(alloc), _vector(NULL), _size(0), _capacity(0) {
+            //     std::cout << CYAN "Constructor with iterators" END << std::endl;
+            //     for (InputIterator it = first; it != last; it++)
+            //         push_back(*it);
+            // }
+            vector(const vector& x) : _alloc(x._alloc), _vector(NULL), _size(0), _capacity(0) {
+                std::cout << CYAN "Copy constructor" END << std::endl;
+                for (size_type i = 0; i < x._size; i++)
+                    push_back(x._vector[i]);
+            }
+            ~vector() {
+                std::cout << RED "Destructor" END << std::endl;
+                for (size_type i = 0; i < _size; i++)
+                    _alloc.destroy(_vector + i);
+                _alloc.deallocate(_vector, _capacity);
+            }
+            // Operators
+            vector& operator=(const vector& x) {
+                std::cout << CYAN "Assignation = operator" END << std::endl;
+                if (this != &x)
+                {
+                    for (size_type i = 0; i < _size; i++)
+                        _alloc.destroy(_vector + i);
+                        
+                    _alloc.deallocate(_vector, _capacity);
+                    _size = x._size;
+                    _capacity = x._capacity;
+                    _vector = _alloc.allocate(_capacity);
+
+                    for (size_type i = 0; i < x._size; i++)
+                        _alloc.construct(_vector + i, x._vector[i]); // push_back(x._vector[i]);
+                }
+                return (*this);
+            }
+            // push_back
+            void push_back(const value_type& val) {
+                if (_size == _capacity)
+                    reserve(_capacity + 1);
+                _alloc.construct(_vector + _size, val);
+                _size++;
+            }
+            // reserve
+            void reserve(size_type n) {
+                if (n > _capacity) {
+                    pointer tmp = _alloc.allocate(n);
+                    for (size_type i = 0; i < _size; i++)
+                        _alloc.construct(tmp + i, _vector[i]);
+                    for (size_type i = 0; i < _size; i++)
+                        _alloc.destroy(_vector + i);
+                    _alloc.deallocate(_vector, _capacity);
+                    _vector = tmp;
+                    _capacity = n;
+                }
             }
     };
 }
