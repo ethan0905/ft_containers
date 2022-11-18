06e4b0a00ca6db6c889e80d8f95ff7a154f3d70f
Ethan SAFAR <esafar@e2r2p9.clusters.42paris.fr>
2022-11-18 17:02:38 +0100

    working on functionnal size test passing tester

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:56:32 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/18 16:24:44 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/18 17:01:56 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,16 @@ namespace ft
         public:
             // Constructors
             explicit vector(const allocator_type& alloc = allocator_type()) : _alloc(alloc), _vector(NULL), _size(0), _capacity(0) { std::cout << CYAN "Default constructor" END << std::endl; }
-            explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _size(n), _capacity(n), _vector(_alloc.allocate(n)){
-                std::cout << CYAN "Constructor with n and val called" END << std::endl;
+            explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc), _vector(_alloc.allocate(n)), _size(n), _capacity(n) {
+                // std::cout << CYAN "Constructor with n and val called" END << std::endl;
                 for (size_type i = 0; i < n; i++)
                     _alloc.construct(_vector + i, val);
             }
             // use enable_if to check if the type is an iterator and is_integral to check if it's an integer
             template<class InputIterator>
 			vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(), typename ft::enable_if<!ft::is_integral<InputIterator>::value>::type* = NULL)
-				: _alloc(alloc), _capacity(0), _size(0), _vector(NULL) {
-                std::cout << CYAN "Constructor with iterators called" END << std::endl;
+				: _alloc(alloc), _vector(NULL), _capacity(0), _size(0) {
+                // std::cout << CYAN "Constructor with iterators called" END << std::endl;
                 InputIterator it = first;
                 
                 for (; it != last; it++)
@@ -58,7 +58,7 @@ namespace ft
                     // _alloc.construct(_vector + _size++, *it);
             }
             vector(const vector& x) : _alloc(x._alloc), _vector(NULL), _size(0), _capacity(0) {
-                std::cout << CYAN "Copy constructor called" END << std::endl;
+                // std::cout << CYAN "Copy constructor called" END << std::endl;
                 _vector = _alloc.allocate(x._size);
                 for (size_type i = 0; i < x._size; i++)
                     _alloc.construct(_vector + i, x._vector[i]);
@@ -66,14 +66,14 @@ namespace ft
                 _capacity = x._capacity;
             }
             ~vector() {
-                std::cout << RED "Destructor called" END << std::endl;
+                // std::cout << RED "Destructor called" END << std::endl;
                 for (size_type i = 0; i < _size; i++)
                     _alloc.destroy(_vector + i);
                 _alloc.deallocate(_vector, _capacity);
             }
             // Operators
             vector& operator=(const vector& x) {
-                std::cout << CYAN "Assignation = operator called" END << std::endl;
+                // std::cout << CYAN "Assignation = operator called" END << std::endl;
                 if (this != &x)
                 {
                     for (size_type i = 0; i < _size; i++)
@@ -169,11 +169,11 @@ namespace ft
                         return (tmp);
                     }
                     iterator operator+(difference_type n) const { 
-                        std::cout << YELLOW "N value: "  << n << END << std::endl;
+                        // std::cout << YELLOW "N value: "  << n << END << std::endl;
                         return (iterator(_ptr + n));
                     }
                     iterator operator-(difference_type n) const { 
-                        std::cout << YELLOW "N value: "  << n << END << std::endl;
+                        // std::cout << YELLOW "N value: "  << n << END << std::endl;
                         return (iterator(_ptr - n));
                     }
                     iterator& operator+=(difference_type n) { 
@@ -318,12 +318,23 @@ namespace ft
             size_type max_size() const {
                 return (_alloc.max_size());
             }
+            size_type capacity() const {
+                return (_capacity);
+            }
+            void resize(size_type n, value_type val = value_type()) {
+                if (n > _size) {
+                    if (n > _capacity)
+                        reserve(n);
+                    for (size_type i = _size; i < n; i++)
+                        _vector[i] = val;
+                }
+                _size = n;
+            }
             void clear() {
                 for (size_type i = 0; i < _size; i++)
                     _alloc.destroy(_vector + i);
                 _size = 0;
             }
-            
     };
 }
 
