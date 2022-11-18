83e1f0c7458d84b4d04292563161f945b9a80264
Ethan SAFAR <esafar@e2r2p9.clusters.42paris.fr>
2022-11-18 16:28:21 +0100

    implemented const_iterators + empty/size/max_size/clear funciton

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:56:32 by esafar            #+#    #+#             */
-/*   Updated: 2022/11/17 15:53:04 by esafar           ###   ########.fr       */
+/*   Updated: 2022/11/18 16:24:44 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,22 +102,12 @@ namespace ft
                 if (n > _capacity) {
                     pointer tmp = _alloc.allocate(n);
                     if (tmp == NULL)
-                    {
-                        std::cout << RED "Allocation failed" END << std::endl;
                         return ;
-                    }
-                    std::cout << "Tmp :" << &tmp << std::endl;
-                    std::cout << "n:" << n << std::endl;
-
                     for (size_type i = 0; i < _size; i++)
                         _alloc.construct(tmp + i, _vector[i]);
                     for (size_type j = 0; j < _size; j++)
                         _alloc.destroy(_vector + j);
-                    std::cout << YELLOW "here" END << std::endl;
-                    std::cout << "Vector :" << &_vector << std::endl;
-                    std::cout << "Capacity :" << _capacity << std::endl;
                     _alloc.deallocate(_vector, _capacity);
-                    std::cout << YELLOW "here" END << std::endl;
                     _vector = tmp;
                     _capacity = n;
                 }
@@ -204,14 +194,134 @@ namespace ft
                         return (*(_ptr + n));
                     }
             };
+
+            // const_iterator      
+            class const_iterator
+            {
+                public:
+                    typedef T value_type;
+                    typedef const T* pointer;
+                    typedef const T& reference;
+                    typedef std::ptrdiff_t difference_type;
+                    typedef std::random_access_iterator_tag iterator_category;
+                private:
+                    pointer _ptr;
+                public:
+                    const_iterator() : _ptr(NULL) {}
+                    const_iterator(pointer ptr) : _ptr(ptr) {}
+                    const_iterator(const const_iterator& it) : _ptr(it._ptr) {}
+                    ~const_iterator() {}
+
+                    const_iterator& operator=(const const_iterator& it)
+                    {
+                        _ptr = it._ptr;
+                        return (*this);
+                    }
+                    bool operator==(const const_iterator& it) const
+                    {
+                        return (_ptr == it._ptr);
+                    }
+                    bool operator!=(const const_iterator& it) const
+                    {
+                        return (_ptr != it._ptr);
+                    }
+                    bool operator<(const const_iterator& it) const
+                    {
+                        return (_ptr < it._ptr);
+                    }
+                    bool operator<=(const const_iterator& it) const
+                    {
+                        return (_ptr <= it._ptr);
+                    }
+                    bool operator>(const const_iterator& it) const
+                    {
+                        return (_ptr > it._ptr);
+                    }
+                    bool operator>=(const const_iterator& it) const
+                    {
+                        return (_ptr >= it._ptr);
+                    }
+                    const_iterator& operator++()
+                    {
+                        _ptr++;
+                        return (*this);
+                    }
+                    const_iterator operator++(int)
+                    {
+                        const_iterator tmp(*this);
+                        _ptr++;
+                        return (tmp);
+                    }
+                    const_iterator& operator--()
+                    {
+                        _ptr--;
+                        return (*this);
+                    }
+                    const_iterator operator--(int)
+                    {
+                        const_iterator tmp(*this);
+                        _ptr--;
+                        return (tmp);
+                    }
+                    const_iterator operator+(difference_type n) const
+                    {
+                        return (const_iterator(_ptr + n));
+                    }
+                    const_iterator operator-(difference_type n) const
+                    {
+                        return (const_iterator(_ptr - n));
+                    }
+                    const_iterator& operator+=(difference_type n)
+                    {
+                        _ptr += n;
+                        return (*this);
+                    }
+                    const_iterator& operator-=(difference_type n)
+                    {
+                        _ptr -= n;
+                        return (*this);
+                    }
+                    reference operator*() const
+                    {
+                        return (*_ptr);
+                    }
+                    pointer operator->() const
+                    {
+                        return (_ptr);
+                    }
+                    reference operator[](difference_type n) const
+                    {
+                        return (*(_ptr + n));
+                    }
+                    
+            };
             //begin and end
             iterator begin() {
-                std::cout << "Begin: " << _vector << std::endl;
                 return (iterator(_vector));
             }
+            const_iterator begin() const {
+                return (const_iterator(_vector));
+            }
             iterator end() {
-                std::cout << "End: " << _vector + _size << std::endl;
                 return (iterator(_vector + _size));
+            }
+            const_iterator end() const {
+                return (const_iterator(_vector + _size));
+            }
+
+            bool empty() const {
+                return (_size == 0);
+            }
+            size_type size() const {
+                return (_size);
+            }
+            size_type max_size() const {
+                return (_alloc.max_size());
+            }
+            void clear() {
+                for (size_type i = 0; i < _size; i++)
+                    _alloc.destroy(_vector + i);
+                _size = 0;
             }
             
     };
