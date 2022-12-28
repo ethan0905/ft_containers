710fa630b4c5ee0dc33538c581f648d2cb073c0e
Safar Ethan <ethansafar@gmail.com>
2022-12-28 13:00:05 +0100

    removed useless files + commented code

--- srcs/containers/vector.hpp
+++ srcs/containers/vector.hpp
@@ -5,15 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/27 14:02:26 by esafar            #+#    #+#             */
-/*   Updated: 2022/12/28 12:29:56 by c2h6             ###   ########.fr       */
+/*   Created: 2022/11/06 17:56:32 by esafar            #+#    #+#             */
+/*   Updated: 2022/12/28 12:57:56 by c2h6             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-# include <memory>  // std::allocator
-# include "../iterators/iterator.hpp"
+#include <memory>  // std::allocator
+#include "../iterators/iterator.hpp"
 #include "../std_functions/enable_if.hpp"
 #include "../std_functions/equal.hpp"
 #include "../std_functions/is_integral.hpp"
@@ -28,7 +28,7 @@ namespace ft {
 			++dist;
 			++first;
 		}
-		return dist;
+		return (dist);
 	}
 	
   template < class T, class Alloc = std::allocator<T> >
@@ -51,14 +51,16 @@ namespace ft {
     private:
 		allocator_type    _alloc;
 		pointer           _ptr;
-		size_type         _capacity;  // 할당된 사이즈
-		size_type         _size;      // 실제로 담겨 있는 데이터의 수
+		size_type         _capacity;
+		size_type         _size;
 
     public:
 		// Constructors
+        // Constructs an empty container, with no elements.
 		explicit vector(const allocator_type& alloc = allocator_type())
 		: _alloc(alloc), _ptr(NULL), _capacity(0), _size(0) {}
 
+		// Constructs a container with n elements. Each element is a copy of val.
 		explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
 		: _alloc(alloc), _ptr(NULL), _capacity(n), _size(n) {
 			_ptr = _alloc.allocate(n);
@@ -66,6 +68,7 @@ namespace ft {
 			_alloc.construct(_ptr + i, val);
 		}
 
+		// Constructs a container with as many elements as the range [first,last), with each element constructed from its corresponding element in that range, in the same order.
 		template <class InputIterator>
 		vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
 		typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type* = NULL)
@@ -77,6 +80,7 @@ namespace ft {
 			push_back(*first);
 		}
 
+		// Copy constructor
 		vector(const vector& x)
 		: _alloc(allocator_type()), _ptr(NULL), _capacity(0), _size(0) {
 			*this = x;
@@ -88,6 +92,7 @@ namespace ft {
 			_alloc.deallocate(_ptr, _capacity);
 		}
 
+		// Assign content
 		vector& operator=(const vector& x) {
 		if (this != &x) {
 			clear();
@@ -147,14 +152,17 @@ namespace ft {
 		_size = n;
 		}
 
+		// Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
 		size_type capacity() const {
 		return (_capacity);
 		}
 
+		// Checks whether the container is empty
 		bool empty() const {
 		return (size() == 0 ? true : false);
 		}
 
+		// Requests that the vector capacity be at least enough to contain n elements.
 		void reserve(size_type n) {
 		if (n > max_size()) {
 			throw (std::length_error("ft::vector::reserve"));
@@ -169,6 +177,7 @@ namespace ft {
 		}
 
 		// Element access
+		// Returns a reference to the element at position n in the vector container.
 		reference operator[](size_type n) {
 		return (_ptr[n]);
 		}
@@ -191,6 +200,7 @@ namespace ft {
 		}
 		}
 
+		// Returns a reference to the first element in the vector.
 		reference front() {
 		return (*begin());
 		}
@@ -198,6 +208,7 @@ namespace ft {
 		return (*begin());
 		}
 
+		// Returns a reference to the last element in the vector.
 		reference back() {
 		return (*(end() - 1));
 		}
@@ -206,6 +217,7 @@ namespace ft {
 		}
 
 		// Modifiers
+		// Assigns new contents to the vector, replacing its current contents, and modifying its size accordingly.
 		template <class InputIterator>
 		void	assign (InputIterator first, InputIterator last, typename enable_if<!is_integral<InputIterator>::value>::type* = 0) {
 			this->clear();
@@ -214,6 +226,7 @@ namespace ft {
 				++first;
 			}
 		}
+		
 		void	assign(size_type n, const value_type& val) {
 			this->clear();
 			while (n) {
@@ -222,6 +235,7 @@ namespace ft {
 			}
 		}
 
+		// Adds a new element at the end of the vector, after its current last element. The content of val is copied (or moved) to the new element.
 		void push_back(const value_type& val) {
 		if (_size == _capacity)
 			empty() ? reserve(1) : reserve(_size * 2);
@@ -229,6 +243,7 @@ namespace ft {
 		_size++;
 		}
 
+		// Removes the last element in the vector, effectively reducing the container size by one.
 		void pop_back() {
 		if (_size > 0) {
 			_alloc.destroy(_ptr + (_size - 1));
@@ -236,6 +251,7 @@ namespace ft {
 		}
 		}
 
+		// The vector is extended by inserting new elements before the element at the specified position, effectively increasing the container size by the number of elements inserted.
 		iterator insert (iterator position, const value_type& val) {
 			size_type n = ft::distance(begin(), position);
 			insert(position, 1, val);
@@ -269,70 +285,77 @@ namespace ft {
 			}
 		}
 
+		// Removes from the vector either a single element (position) or a range of elements ([first,last]).
 		iterator erase(iterator position) {
-		if (empty()) return (end());
-		iterator it = begin();
-		while (it != position) it++;
-		_alloc.destroy(&(*it));
-		while (it + 1 != end()) {
-			_alloc.construct(&(*it), *(it + 1));
-			it++;
-		}
-		pop_back();
-		return (position);
+			if (empty())
+				return (end());
+			iterator it = begin();
+			while (it != position)
+				it++;
+			_alloc.destroy(&(*it));
+			while (it + 1 != end()) {
+				_alloc.construct(&(*it), *(it + 1));
+				it++;
+			}
+			pop_back();
+			return (position);
 		}
 		iterator erase(iterator first, iterator last) {
-		if (empty()) return (end());
-		iterator it = begin();
-		iterator rtn = first;
-		difference_type dist = last - first;
-		while (it != first) it++;
-		for (; first != last; first++)
-			_alloc.destroy(&(*first));
-		while (it + dist != end()) {
-			_alloc.construct(&(*it), *(it + dist));
-			it++;
-		}
-		while (dist-- > 0) pop_back();
-		return (rtn);
+			if (empty())
+				return (end());
+			iterator it = begin();
+			iterator rtn = first;
+			difference_type dist = last - first;
+			while (it != first)
+				it++;
+			for (; first != last; first++)
+				_alloc.destroy(&(*first));
+			while (it + dist != end()) {
+				_alloc.construct(&(*it), *(it + dist));
+				it++;
+			}
+			while (dist-- > 0)
+				pop_back();
+			return (rtn);
 		}
 
 		void swap(vector& x) {
-		allocator_type    tmp_alloc = _alloc;
-		pointer           tmp_ptr = _ptr;
-		size_type         tmp_capacity = _capacity;
-		size_type         tmp_size = _size;
+			allocator_type    tmp_alloc = _alloc;
+			pointer           tmp_ptr = _ptr;
+			size_type         tmp_capacity = _capacity;
+			size_type         tmp_size = _size;
 
-		_alloc = x._alloc;
-		_ptr = x._ptr;
-		_capacity = x._capacity;
-		_size = x._size;
+			_alloc = x._alloc;
+			_ptr = x._ptr;
+			_capacity = x._capacity;
+			_size = x._size;
 
-		x._alloc = tmp_alloc;
-		x._ptr = tmp_ptr;
-		x._capacity = tmp_capacity;
-		x._size = tmp_size;
+			x._alloc = tmp_alloc;
+			x._ptr = tmp_ptr;
+			x._capacity = tmp_capacity;
+			x._size = tmp_size;
 		}
 
 		void clear() {
-		if (_size > 0) {
-			for (iterator it = begin(); it != end(); it++)
-			_alloc.destroy(&(*it));
-			_size = 0;
-		}
+			if (_size > 0) {
+				for (iterator it = begin(); it != end(); it++)
+				_alloc.destroy(&(*it));
+				_size = 0;
+			}
 		}
 
 		// Allocator
 		allocator_type get_allocator() const {
-		return (Alloc(_alloc));
+			return (Alloc(_alloc));
 		}
 
 		private:
-		void move_backward(iterator position, size_type n = 1) {
-		if (position == end()) return ;
-			move_backward(position + 1, n);
-		_alloc.construct(&(*(position + n)), *position);
-		}
+			void move_backward(iterator position, size_type n = 1) {
+				if (position == end())
+					return ;
+				move_backward(position + 1, n);
+				_alloc.construct(&(*(position + n)), *position);
+			}
 	};
 
 	// Non-member function overloads
